b0a74acb7902824afb38b7aa95c4608dcd21a55b
HasithaErandika <165554609+HasithaErandika@users.noreply.github.com>
2024-04-03 06:12:23 +0530

    Update teacher-add.test.js 1

--- frontend/test/teacher-add.test.js
+++ frontend/test/teacher-add.test.js
@@ -13,7 +13,7 @@ test('Testing add teachers', async t => {
     await t.typeText("#teacher-age", "45");
     await t.click("#teacher-add");
 
-    await t.navigateTo("/teacher");
+    await t.navigateTo("/");
 
     const table = Selector('#teacher-table')
     const rowCount = await table.find('tr').count;
